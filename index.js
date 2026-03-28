bc259154fbaa54e320c2a3686aff92b47418d046
adeputranr <adeputranr3@gmail.com>
2026-03-28 13:53:22 +0800

    index.js

---
+++ index.js
@@ -0,0 +1,128 @@
+const express = require('express');
+const TelegramBot = require('node-telegram-bot-api');
+
+const app = express();
+app.use(express.json());
+
+// ==============================
+// CONFIG
+// ==============================
+const token = "8177406788:AAFO7SpjNM1DYeSzMgpXbulZGNvqSrNG-5c";
+const GROUP_ID = "-1003773056558";
+
+// ==============================
+// INIT BOT
+// ==============================
+const bot = new TelegramBot(token, { polling: true });
+
+// SIMPAN USER
+const users = {};
+
+
+// ==============================
+// START COMMAND
+// ==============================
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id,
+`🔥 VIP Mas Ade 🔥
+Harga: 100K
+
+Klik tombol di bawah untuk lanjut:`,
+  {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: "Beli VIP Mas Ade (100K)", callback_data: "VIP" }]
+      ]
+    }
+  });
+});
+
+
+// ==============================
+// USER KLIK
+// ==============================
+bot.on("callback_query", (query) => {
+  const chatId = query.message.chat.id;
+
+  const orderId = Date.now().toString();
+  users[orderId] = chatId;
+
+  const paymentLink = `https://saweria.co/masadeeee`;
+
+  bot.sendMessage(chatId,
+`💰 Pembayaran VIP Mas Ade (100K)
+
+Klik link:
+${paymentLink}
+
+📌 WAJIB:
+Isi pesan saat donasi dengan kode berikut:
+
+${orderId}
+
+⚠️ Nominal harus MINIMAL 100.000`
+  );
+});
+
+
+// ==============================
+// WEBHOOK SAWERIA
+// ==============================
+app.post('/webhook', async (req, res) => {
+  try {
+    console.log("DATA MASUK:", req.body);
+
+    const amount = req.body.amount_raw || 0;
+    const message = req.body.message || "";
+
+    const orderId = message.trim();
+    const telegram_id = users[orderId];
+
+    if (!telegram_id) {
+      console.log("User tidak ditemukan:", orderId);
+      return res.sendStatus(200);
+    }
+
+    if (amount < 100000) {
+      await bot.sendMessage(
+        telegram_id,
+        `❌ Pembayaran kamu kurang dari 100K
+
+Silakan ulangi pembayaran sesuai nominal:
+100.000`
+      );
+      return res.sendStatus(200);
+    }
+
+    const invite = await bot.createChatInviteLink(
+      GROUP_ID,
+      { member_limit: 1 }
+    );
+
+    await bot.sendMessage(
+      telegram_id,
+      `✅ Pembayaran berhasil!
+
+Ini link VIP kamu:
+${invite.invite_link}`
+    );
+
+    delete users[orderId];
+
+    res.sendStatus(200);
+
+  } catch (err) {
+    console.log("ERROR:", err.message);
+    res.sendStatus(500);
+  }
+});
+
+
+// ==============================
+// RUN SERVER
+// ==============================
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server jalan di port", PORT);
+});
